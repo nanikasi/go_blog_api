2df3f6e22c6f3473d82896dd915db8c014a4ab97
Naoto Nakanishi <nanikasi1024@gmail.com>
2023-10-21 14:25:19 +0900

    make hello path

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 		io.WriteString(w, "Hello, World!\n")
 	}
 
-	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/hello", helloHandler)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
